c216d9fbd7788e9aac67244268cece08cac0542b
Artyom Trukhan <artidnb@gmail.com>
2022-04-20 20:07:51 +0300

    Добавляет анимацию открытия-закрытия мобильного меню

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Collapse component
 (function () {
   const collapseTitles = document.querySelectorAll('.collaps__title');
 
@@ -8,55 +9,91 @@
     collapseBtn.addEventListener('click', () => {
       let expanded = collapseBtn.getAttribute('aria-expanded') === 'true';
       collapseBtn.setAttribute('aria-expanded', !expanded);
-      collapseBtn.classList.toggle('collaps__btn--open');
       target.classList.toggle('collaps__desc--show');
     });
   });
 })();
 
+// Mobile menu
 (function () {
   const burgerMenuBtn = document.querySelector('.burger-btn');
   const sideMenu = document.querySelector('.main-nav__list-wrap');
-  const closeMenuBtn = document.querySelector('.close-btn');
+  const sideMenuLinks = sideMenu.querySelectorAll('.main-nav__link');
+  const closeMenuBtn = sideMenu.querySelector('.close-btn');
+
+  function hideMenu() {
+    sideMenu.setAttribute('aria-hidden', 'true');
+    sideMenuLinks.forEach((sidemenuLink) => {
+      sidemenuLink.setAttribute('tabindex', '-1');
+    });
+    closeMenuBtn.setAttribute('tabindex', '-1');
+  }
+
+  function showMenu() {
+    sideMenu.setAttribute('aria-hidden', 'false');
+    sideMenuLinks.forEach((sidemenuLink) => {
+      sidemenuLink.setAttribute('tabindex', '0');
+    });
+    closeMenuBtn.setAttribute('tabindex', '0');
+  }
+
+  function checkScreen() {
+    if (window.matchMedia('(max-width: 1024px)').matches) {
+      hideMenu();
+    } else {
+      showMenu();
+    }
+  }
+
+  window.addEventListener('resize', () => {
+    checkScreen();
+  });
+
+  document.addEventListener('DOMContentLoaded', () => {
+    checkScreen();
+  });
 
   burgerMenuBtn.addEventListener('click', () => {
     let expanded = burgerMenuBtn.getAttribute('aria-expanded') === 'true';
     burgerMenuBtn.setAttribute('aria-expanded', !expanded);
     sideMenu.classList.add('main-nav__list-wrap--show');
+    showMenu()
   });
 
   closeMenuBtn.addEventListener('click', () => {
     sideMenu.classList.remove('main-nav__list-wrap--show');
+    hideMenu();
   });
+})();
 
-  const focusableElements = 'button, [href], [tabindex]:not([tabindex="-1"])';
-  const firstFocusableElement = sideMenu.querySelectorAll(focusableElements)[0];
-  const focusableContent = sideMenu.querySelectorAll(focusableElements);
-  const lastFocusableElement = focusableContent[focusableContent.length - 1];
+// Focus trap
+(function () {
+  function trapFocus(element) {
+    const focusableElements = element.querySelectorAll('a[href], button, [tabindex]:not([tabindex="-1"])');
+    const firstFocusableElement = focusableElements[0];
+    const lastFocusableElement = focusableElements[focusableElements.length - 1];
+    const KEYCODE_TAB = 9;
 
-  document.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('resize', () => {
-      if (window.matchMedia('(max-width: 1024px)').matches) {
-        document.addEventListener('keydown', (evt) => {
-          let isTabPressed = evt.key === 'Tab' || evt.keyCode === 9;
-          if (!isTabPressed) {
-            return;
-          }
-          if (evt.shiftKey) {
-            if (document.activeElement === firstFocusableElement) {
-              lastFocusableElement.focus();
-              evt.preventDefault();
-            }
-          } else {
-            if (document.activeElement === lastFocusableElement) {
-              firstFocusableElement.focus();
-              evt.preventDefault();
-            }
-          }
-        });
-      
-        firstFocusableElement.focus();
+    element.addEventListener('keydown', function(evt) {
+      let isTabPressed = (evt.key === 'Tab' || evt.keyCode === KEYCODE_TAB);
+  
+      if (!isTabPressed) { 
+        return; 
       }
+  
+      if (evt.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          lastFocusableElement.focus();
+          evt.preventDefault();
+          }
+        } else {
+        if (document.activeElement === lastFocusableElement) {
+          firstFocusableElement.focus();
+            evt.preventDefault();
+          }
+        }
     });
-  });
+  }
+  const sideMenu = document.querySelector('.main-nav__list-wrap');
+  trapFocus(sideMenu);
 })();
